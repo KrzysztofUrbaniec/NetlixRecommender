780cbd2ea4f6310dccafc745899163b815206a8d
Krzysztof Urbaniec <urbaniec.krzysztof@interia.pl>
2024-06-27 17:58:40 +0200

    Updated utility functions for modelling

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
 
-from surprise.accuracy import rmse, mae
+from surprise.model_selection import GridSearchCV
+from surprise.model_selection import LeaveOneOut
 
+from scripts.Metrics import Metrics
+
+# ----------------------------------------------- Exploration.ipynb ----------------------------------------------
 def get_sample_data(ratings, n_users, n_movies, random_state=0):
     np.random.seed(random_state)
 
@@ -25,6 +29,13 @@ def get_sample_data(ratings, n_users, n_movies, random_state=0):
 
     return sample
 
+def print_sample_summary(sample):
+    print(f"Actual number of unique users in the sample: {sample['CustId'].unique().size}")
+    print(f"Actual number of unique movies in the sample: {sample['MovieId'].unique().size}")
+    print(f"Total number of ratings in the sample: {len(sample)}")
+
+# ----------------------------------------------- Modeling.ipynb ----------------------------------------------
+
 def evaluate_baseline_performance(algo, train, test, verbose=True):
 
     if verbose is True: print('Training the model and making predictions...')
@@ -32,8 +43,8 @@ def evaluate_baseline_performance(algo, train, test, verbose=True):
     predictions = algo.test(test)
 
     if verbose is True: print('Computing RMSE and MAE...')
-    rmse_test = rmse(predictions, verbose=True)
-    mae_test = mae(predictions, verbose=True)
+    rmse_test = Metrics.RMSE(predictions, verbose=verbose)
+    mae_test = Metrics.MAE(predictions, verbose=verbose)
     
     if verbose is True: print('\nDone.')
 
@@ -47,4 +58,51 @@ def test_algorithms(algorithms: dict, train, test, verbose=True):
         rmse_algo, mae_algo = evaluate_baseline_performance(algo, train, test, verbose=verbose)
         results[name] = [rmse_algo, mae_algo]
         print('')
-    return pd.DataFrame(results, index=['RMSE', 'MAE']).T.sort_values(by='RMSE').round(4) 
+    results_df = pd.DataFrame(results, index=['RMSE', 'MAE']).T.sort_values(by='RMSE').round(4) 
+    # best_algo = algorithms[results_df.iloc[0].name]
+    return results_df
+
+def grid_search(data, algo_class, param_grid, measures=['rmse','mae'], cv=3, n_jobs=-1, joblib_verbose=3):
+    
+    grid_search = GridSearchCV(algo_class, param_grid, measures=measures, cv=cv, n_jobs=n_jobs, joblib_verbose=joblib_verbose)
+
+    print('Initializing the search...')
+    grid_search.fit(data)
+
+    print('Extracting best parameters and scores...')
+    print('\nDone.')
+    return grid_search.best_params, grid_search.best_score
+
+def predict_top_n_recs(algo, ratings, n=10, random_state=0):
+    LOOCV = LeaveOneOut(n_splits=1, random_state=random_state)
+    for train, test in LOOCV.split(ratings):
+        algo.fit(train)
+        leftOutPredictions = algo.test(test) 
+        bigTestSet = train.build_anti_testset()
+        allPredictions = algo.test(bigTestSet)
+        topN = Metrics.GetTopN(allPredictions, n=n)
+    return leftOutPredictions, topN
+
+def compute_topN_metrics(left_out_predictions, topN, popranks, simsAlgo, n_users, print_metrics=True):
+    metrics = dict()
+    metrics['HR'] = Metrics.HitRate(topN, left_out_predictions)
+    metrics['cHR'] = Metrics.CumulativeHitRate(topN, left_out_predictions, ratingCutoff=4.0)
+    metrics['ARHR'] = Metrics.AverageReciprocalHitRank(topN, left_out_predictions)
+    metrics['Diversity'] = Metrics.Diversity(topN, simsAlgo)
+    metrics['User Coverage'] = Metrics.UserCoverage(topN, n_users, ratingThreshold=4.0)
+    metrics['Novelty'] = Metrics.Novelty(topN, popranks)
+    
+    if print_metrics:
+        for metric, value in metrics.items():
+            print(f'{metric} = {value:4f}')
+
+        print("\nLegend:\n")    
+        print("HR:        Hit Rate; how often we are able to recommend a left-out rating. Higher is better.")
+        print("cHR:       Cumulative Hit Rate; hit rate, confined to ratings above a certain threshold. Higher is better.")
+        print("ARHR:      Average Reciprocal Hit Rank - Hit rate that takes the ranking into account. Higher is better." )
+        print("Diversity: 1-S, where S is the average similarity score between every possible pair of recommendations")
+        print("           for a given user. Higher means more diverse.")
+        print("User Coverage:  Ratio of users for whom recommendations above a certain threshold exist. Higher is better.")
+        print("Novelty:   Average popularity rank of recommended items. Higher means more novel.")
+
+    return metrics
